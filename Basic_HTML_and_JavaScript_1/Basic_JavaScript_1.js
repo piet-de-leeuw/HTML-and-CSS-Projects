0e242d8c496c3583a7522678d62479b3ded69fca
piet-de-leeuw <pietdeleeuw156@gmail.com>
2024-04-30 10:39:33 +0200

    Added the files Basic_HTML_1.html, Basic_HTML_2.html, Basic_JavaScript_1.js and Dog.jpg

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+alert("This is a HTML practice");
+
+	function My_First_Function() 
+	{
+		var string = "You clickt Me!";
+		var result = string.fontcolor("green");
+		document.getElementById("Click").innerHTML =
+		result;
+	}
